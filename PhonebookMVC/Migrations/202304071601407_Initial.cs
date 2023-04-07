d97257db7feef6b7d8f71bbc7cd8abb63424b0b6
Alexandra Valkova <alexandra.valkova.97@gmail.com>
2023-04-07 18:38:31 +0200

    :card_file_box: :seedling: Added Migrations and fixed Seed method

---
+++ PhonebookMVC/Migrations/202304071601407_Initial.cs
@@ -0,0 +1,97 @@
+﻿namespace PhonebookMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        UserID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Users", t => t.UserID, cascadeDelete: true)
+                .Index(t => t.UserID);
+            
+            CreateTable(
+                "dbo.Groups",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        UserID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Users", t => t.UserID, cascadeDelete: true)
+                .Index(t => t.UserID);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Username = c.String(),
+                        Password = c.String(),
+                        Email = c.String(),
+                        IsAdmin = c.Boolean(nullable: false),
+                        ImageName = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Phones",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        PhoneNumber = c.String(),
+                        ContactID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Contacts", t => t.ContactID, cascadeDelete: true)
+                .Index(t => t.ContactID);
+            
+            CreateTable(
+                "dbo.GroupContacts",
+                c => new
+                    {
+                        Group_ID = c.Int(nullable: false),
+                        Contact_ID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Group_ID, t.Contact_ID })
+                .ForeignKey("dbo.Groups", t => t.Group_ID)
+                .ForeignKey("dbo.Contacts", t => t.Contact_ID)
+                .Index(t => t.Group_ID)
+                .Index(t => t.Contact_ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Contacts", "UserID", "dbo.Users");
+            DropForeignKey("dbo.Phones", "ContactID", "dbo.Contacts");
+            DropForeignKey("dbo.Groups", "UserID", "dbo.Users");
+            DropForeignKey("dbo.GroupContacts", "Contact_ID", "dbo.Contacts");
+            DropForeignKey("dbo.GroupContacts", "Group_ID", "dbo.Groups");
+            DropIndex("dbo.GroupContacts", new[] { "Contact_ID" });
+            DropIndex("dbo.GroupContacts", new[] { "Group_ID" });
+            DropIndex("dbo.Phones", new[] { "ContactID" });
+            DropIndex("dbo.Groups", new[] { "UserID" });
+            DropIndex("dbo.Contacts", new[] { "UserID" });
+            DropTable("dbo.GroupContacts");
+            DropTable("dbo.Phones");
+            DropTable("dbo.Users");
+            DropTable("dbo.Groups");
+            DropTable("dbo.Contacts");
+        }
+    }
+}
--- PhonebookMVC/Migrations/Configuration.cs
+++ PhonebookMVC/Migrations/Configuration.cs
@@ -1,4 +1,4 @@
-namespace PhonebookMVC.Migrations
+﻿namespace PhonebookMVC.Migrations
 {
     using Models;
     using System.Data.Entity.Migrations;
@@ -8,27 +8,25 @@ namespace PhonebookMVC.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(PhonebookMvcDB context)
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
 
             context.Users.AddOrUpdate(
                 u => u.Username,
-                new User { Username = "admin", Password = "password", IsAdmin = true }
+                new User
+                {
+                    FirstName = "Admin",
+                    LastName = "Adminov",
+                    Username = "admin",
+                    Password = "password",
+                    IsAdmin = true
+                }
             );
         }
     }
